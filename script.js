82a9a4c5e6d444264498e0ba765a784136942ec9
CesarRosales16 <00060917@uca.edu.sv>
2018-10-26 22:01:36 -0600

    Terminado

--- script.js
+++ script.js
@@ -56,7 +56,26 @@ console.log("Average age %i", list.reduce((sum, {
 function getAge(birthday) {
     var ageDifMs = Date.now() - birthday;
     var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
+    var edad = Math.abs(ageDate.getUTCFullYear() - 1970);
+    //El codigo original calculaba la fecha independientemente del mes y del dia de nacimiento
+    /*
+     *Si el mes de la fecha del cumpleaños aun no ha pasado (es mayor) que el mes de la fecha actual es porque
+     *el cumpleaños no ha pasado por lo tanto se le resta un año a la edad
+    */
+    if (ageDate.getMonth > new Date().getMonth) {
+        edad--;
+    }
+    /*
+     * Si el mes es el mismo, la edad la determina el dia de cumpleaños
+     * si el dia del cumpleaños es menor al dia actual el cumpleaños no ha pasado, por lo tanto se le resta un año a la edad
+     */
+    if (ageDate.getMonth == new Date().getMonth) {
+        if (ageDate.getDay < new Date().getDay) {
+            edad--;
+        }
+    }
+    //Finalmente se retorna la edad real de la persona donde se considero el año, es y dia de su cumpleaños
+    return edad;
 }
 
 console.log("Average age %i", list.reduce((sum, {
